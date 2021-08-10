aedaad7dc50aa683261e9d8aae22d57c3541e86a
Coco-hjay <huangjiegogogo@gmail.com>
2021-08-10 17:08:58 +0800

    edit bitmex

--- ccxt/bitmex.py
+++ ccxt/bitmex.py
@@ -11,7 +11,16 @@ if 'test' in bitmex.urls:
         'http': 'http://127.0.0.1:10809',
         'https': 'http://127.0.0.1:10809'
     }
+       
+bitmex.load_markets()
 
 # print(json.dumps(bitmex.load_markets()['BTC/USD'], indent=4))
-bitmex.load_markets()
-print(json.dumps(bitmex.fetch_order_book('BTC/USD'), indent=4))
+# orderbook = bitmex.fetch_order_book('BTC/USD')
+# bid = orderbook['bids'][0][0] if len (orderbook['bids']) > 0 else None
+# ask = orderbook['asks'][0][0] if len (orderbook['asks']) > 0 else None
+# spread = (ask - bid) if (bid and ask) else None
+# print (bitmex.id, 'market price', { 'bid': bid, 'ask': ask, 'spread': spread })
+
+# print(json.dumps(bitmex.fetch_ticker('BTC/USD'), indent=4))
+
+print(bitmex.fetch_balance())
